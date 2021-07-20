630d91e4cf94f31cd52e0b69396a78206fc9a7fe
Jérôme Clament-Sanz <j.clamentsanz@gmail.com>
2021-07-20 12:50:35 +0200

    refacto

--- app/src/main/java/com/example/stackoverflowapp/repository/RemoteRepository.kt
+++ app/src/main/java/com/example/stackoverflowapp/repository/RemoteRepository.kt
@@ -6,10 +6,10 @@ import retrofit2.Response
 object RemoteRepository {
 
     suspend fun <T> safeApiCall(call: suspend () -> Response<T>): ApiResult<T> {
-        val myResp: Response<T> = call() // invoke() est une méthode de coroutines
+        val myResp: Response<T> = call() // on invoque notre lambda/coroutine entrée en paramètre qui nous retourne un objet Response
         return when {
-            myResp.isSuccessful -> ApiResult.Success(myResp.body()!!)
-            else -> ApiResult.Error("Error : not successful")
+            myResp.isSuccessful -> ApiResult.Success(myResp.body()!!) // si la réponse est successful, on retourne un objet ApiResult.Success
+            else -> ApiResult.Error("Error : not successful") // sinon on retourne un objet ApiResult.Error
         }
     }
 
